f1dff3c5c4b1a4f2a6fd100878eeda641e19f4af
Fer-117 <fsg11497@gmail.com>
2023-10-16 17:53:53 -0400

    segunda version de página

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-window.onload = () => {
-  setTimeout(() => {
-    document.querySelector('body').classList.add('display');
-  }, 4000);
-};
+const buttons = document.querySelectorAll('[data-carousel-button]');
 
-document.querySelector('.hamburger-menu').addEventListener('click', () => {
-  document.querySelector('.container').classList.toggle('change');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselBtn === 'next' ? 1 : -1;
+    const slides = button
+      .closest('[data-carousel]')
+      .querySelector('[data-slides]');
+
+    const activeSlide = slides.querySelector('[data-active]');
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
 });
-
-document.querySelector('.scroll-btn').addEventListener('click', () => {
-  document.querySelector('html').style.scrollBehavior = 'smooth';
-  setTimeout(() => {
-    document.querySelector('html').style.scrollBehavior = 'unset';
-  }, 1000);
-});
-
